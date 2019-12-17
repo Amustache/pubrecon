c5a1486c9d6bd8d73bdc5cdec7dbfd8aebff5a87
Stache <hugo.hueber@epfl.ch>
2019-12-17 15:00:13 +0100

    More packaging

--- project2/setup.py
+++ project2/setup.py
@@ -2,21 +2,21 @@ import setuptools
 
 
 def readme():
-    with open('README.rst') as f:
+    with open('README.md') as f:
         return f.read()
 
 
 setuptools.setup(name='pubrecon',
-      version='0.1',
-      author="Hugo 'Stache' Hueber",
-      author_email="hugo.hueber@epfl.ch",
-      license='MIT',
-      description='Ad recognition',
-      long_description=readme(),
-      long_description_content_type="text/markdown",
-      url='http://github.com/AMustache/ML-2019/project2/',
-      packages=setuptools.find_packages(),
-      install_requires=['pandas'],
-      include_package_data=True,
-      zip_safe=False
-)
+                 version='0.1',
+                 author="Hugo 'Stache' Hueber",
+                 author_email="hugo.hueber@epfl.ch",
+                 license='MIT',
+                 description='Ad recognition',
+                 long_description=readme(),
+                 long_description_content_type="text/markdown",
+                 url='http://github.com/AMustache/ML-2019/project2/',
+                 packages=setuptools.find_packages(),
+                 install_requires=['cv2', 'keras', 'numpy', 'tqdm', 'pandas', 'sklearn'],
+                 include_package_data=True,
+                 zip_safe=False
+                 )
