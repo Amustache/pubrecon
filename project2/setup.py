215cae37bdd5891ef8feb76ba0725e7958ca2870
Stache <Amustache@users.noreply.github.com>
2019-12-19 21:19:09 +0100

    Update setup.py

--- project2/setup.py
+++ project2/setup.py
@@ -21,5 +21,5 @@ setuptools.setup(name='pubrecon',
                                    'pandas==0.25.3', 'sklearn'],
                  include_package_data=True,
                  zip_safe=False,
-                 python_requires='==3.7',
+                 python_requires='==3.7.5',
                  )
